b97e746a94be1c546f14698cca58a55ad0e2ace2
czarakas <czarakas@uw.edu>
2022-02-08 12:59:44 -0700

    Run script for spin up

---
+++ run_scripts/run_defaultParams_SpinUp.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+#############################################################################################################################
+
+#############################################################################################################################
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Define directories and user settings
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+export CESM_CASE_NAME=spinup_test003 #coupled_BGC_defParams_SpinUp
+export CESM_CASE_RES=f19_g17
+export CESM_COMPSET=2010_CAM60_CLM50%BGC_CICE_DOCN%SOM_SROF_SGLC_SWAV
+export PROJECT_NUM=UWAS0044
+
+export CESM_SRC_DIR=/glade/u/home/czarakas/cesm_source/cesm_coupled_PPE   #codebase to use to run CESM
+export CESM_CASE_DIR=/glade/u/home/czarakas/cesm_cases/coupled_PPE #where to save case
+export ARCHIVE_DIR=/glade/scratch/czarakas/archive/ #where to save output
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Make and configure case
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Change to directory to make case
+cd ${CESM_SRC_DIR}/cime/scripts
+
+# Create new case
+./create_newcase --case ${CESM_CASE_DIR}/${CESM_CASE_NAME} --res ${CESM_CASE_RES} --compset ${CESM_COMPSET} --project ${PROJECT_NUM} --run-unsupported
+
+#+++ Configure case
+cd ${CESM_CASE_DIR}/${CESM_CASE_NAME}
+
+#+++ Modify xml files
+./xmlchange STOP_OPTION="ndays"
+./xmlchange STOP_N=3
+./xmlchange RESUBMIT=0
+./xmlchange DOCN_SOM_FILENAME="pop_frc.b.e21.BW1850.f09_g17.CMIP6-piControl.001.190514.nc"
+./xmlchange JOB_WALLCLOCK_TIME=00:30:00 --subgroup case.run
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# set up and compile case
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Set up case
+./case.setup
+
+# Change run settings
+#./xmlchange CCSM_CO2_PPMV=284.7
+./xmlchange CLM_CO2_TYPE="constant"
+
+# Modify namelists
+cat >> user_nl_clm << EOF
+! ---------------------------------INITIAL CONDITIONS------------------------------
+finidat ="/glade/p/cgd/tss/people/oleson/CLM5_restarts/clm51_PPEn02ctsm51d021_2deg_GSWP3V1_leafbiomassesai_PPE3_1850pAD.clm2.r.2041-01-01-00000.nc"
+use_init_interp = .false.
+
+! Default parameter file used in PPE
+paramfile = "/glade/p/cgd/tss/people/oleson/modify_param/ctsm51_params.c210217_kwo.c210222.nc"
+
+!----------------------------------------------------------------------------------
+!------------------------------HISTORY FILES--------------------------------------
+!----History files (h0): monthly output (all the default output variables)
+
+!----History files (h1): monthly output (less variables - the spin up that we will actually back up)
+hist_fincl2 += 'TLAI','TSAI','HBOT','HTOP','LAISUN','LAISHA'
+hist_fincl2 += 'EFLX_LH_TOT','FIRA','FIRE','FLDS','FSA','FSDS','FSH_TO_COUPLER','FSR','FCEV','FCTR','FGEV'
+hist_fincl2 += 'GPP','NEE','NPP','ER','AR','GR','HR','MR','SR'
+hist_fincl2 += 'TOTCOLC','TOTCOLN','TOTECOSYSC','TOTECOSYSN','TOTSOMC','TOTSOMN','TOTVEGC','TOTVEGN'
+hist_fincl2 += 'RAIN_FROM_ATM','TBOT','QBOT','THBOT','TSA','TSKIN','TV','WIND','PBOT','PCO2','RH2M','SNOW_FROM_ATM'
+hist_fincl2 += 'FSNO','DSTFLXT','DSTDEP','BTRAN2','QRUNOFF_TO_COUPLER','TWS'
+
+
+hist_nhtfrq = -24, -24        !use this for short test case
+!hist_nhtfrq = 0,0              !use this for actual spin up
+hist_mfilt=120,120
+
+EOF
+
+
+# Build the case
+cd ${CESM_CASE_DIR}/${CESM_CASE_NAME}
+qcmd -A ${PROJECT_NUM} -- ./case.build
+
+# Submit the case
